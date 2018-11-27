93fc99e43b3ef76bb8804d6a754e233a3cfa9ca9
Yrroth <charles.goodling@gmail.com>
2018-11-27 15:24:27 -0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="stylesheet" type="text/css" href="style.css"/>
+
+	<head>
+		<meta charset="utf-8"/>
+		<title><?php echo $title="Homepage" ;?> </title>
+	
+	</head>
+		<body>
+			<div class= "wrapper">
+			
+<header>
+	
+	
+		<p> <a href="login.php">Home Page</a></p>
+	</header>
+	<div class="main">
+	
