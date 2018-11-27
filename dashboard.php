9027dcacd510e466c134fd2a7d7e7073965754a5
Yrroth <charles.goodling@gmail.com>
2018-11-27 15:28:20 -0500

    Create dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,29 @@
+<?php include "header.php";?>
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+ 
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="stylesheet" type="text/css" href="style.css"/>
+
+</head>
+<body>
+
+//((select * from 'persons' INNER JOIN department on persDept=deptid))//
+look at lab 11 
+
+
+
+
+
+
+
+
+
+</body>
+</html>
